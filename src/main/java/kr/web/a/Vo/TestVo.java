5d371afba8c9ab0d9487f0f0067b43f4f07f678a
AkkeSun <akkessun@gmail.com>
2021-07-20 13:23:17 +0900

    0720 aop test(fail)

--- src/main/java/kr/web/a/Vo/TestVo.java
+++ src/main/java/kr/web/a/Vo/TestVo.java
@@ -9,4 +9,11 @@ public class TestVo implements Serializable{
 	private int val1;
 	private String val2;
 	private String val3;
+
+
+	public void getCatsInfo() {
+		System.out.println("val1 : " + getVal1());
+		System.out.println("val2 : " + getVal2());
+		System.out.println("val3 : " + getVal3());
+	}
 }
